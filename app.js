bfbb736e9aa5a113ebf4cce31d04cda1ca0d007d
Eric Porter <eporter@scentsy.com>
2016-05-18 15:53:29 -0600

    Added functionality to give stick figures different name-state and graphic as health depletes. Also added a reset button to start the game over.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var health = 100;
-var name = "Bad Joe";
+var name = "Joe";
 var hits = 0;
 
 var slap = function () {
@@ -24,10 +24,22 @@ var update = function () {
 document.getElementById("health").innerText = health;
 document.getElementById("hits").innerText = hits;
 document.getElementById("name").innerText = name;
+    if (health <= 50) {
+   document.getElementById("stick-fig").src = ("stick-fig-bad.jpg");
+   document.getElementById("name").innerText = "Bad Joe";
+}
     if (health <= 0) {
    document.getElementById("stick-fig").src = ("stick-fig-dead.jpg");
+   document.getElementById("name").innerText = "Dead Joe";
 }
 }
 
+var reset = function () {
+    health = 100;
+    hits = 0;
+    document.getElementById("stick-fig").src = ("stick-fig.jpg");
+    document.getElementById("name").innerText = "Joe";
+    update();
+}
 
 update();
