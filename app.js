8cb7a1a4d798f7fef90ff6208c66acba259d0c14
Eric Porter <eporter@scentsy.com>
2016-05-18 16:46:10 -0600

    Added reaction states and game over message

--- app.js
+++ app.js
@@ -5,18 +5,30 @@ var hits = 0;
 var slap = function () {
     health -= 1;
     hits += 1;
+    document.getElementById("reaction").innerText = "Hey!";
+    if (health <=50) {
+        document.getElementById("reaction").innerText = "Ouch!";
+    }
     update();
 }
 
 var punch = function () {
     health -= 5;
     hits += 1;
+    document.getElementById("reaction").innerText = "Rude!";
+    if (health <=50) {
+        document.getElementById("reaction").innerText = "Oof!";
+    }
     update();
 }
 
 var kick = function () {
     health -= 10;
     hits += 1;
+    document.getElementById("reaction").innerText = "What was that for?";
+    if (health <=50) {
+        document.getElementById("reaction").innerText = "Ugh!";
+    }
     update();
 }
 
@@ -31,6 +43,7 @@ document.getElementById("name").innerText = name;
     if (health <= 0) {
    document.getElementById("stick-fig").src = ("stick-fig-dead.jpg");
    document.getElementById("name").innerText = "Dead Joe";
+   document.getElementById("reaction").innerText = "Well, he's dead now. What do you have to say for yourself?";
 }
 }
 
@@ -39,6 +52,7 @@ var reset = function () {
     hits = 0;
     document.getElementById("stick-fig").src = ("stick-fig.jpg");
     document.getElementById("name").innerText = "Joe";
+    document.getElementById("reaction").innerText = "";
     update();
 }
 
